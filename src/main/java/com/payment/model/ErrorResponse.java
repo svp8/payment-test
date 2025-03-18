74f0fd9c75940708553d18c8ba8a7731c5db05f3
Vlad <vlad-pvs@mail.ru>
2025-03-18 18:38:02 +0500

    error format

--- src/main/java/com/payment/model/ErrorResponse.java
+++ src/main/java/com/payment/model/ErrorResponse.java
@@ -2,7 +2,18 @@ package com.payment.model;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class ErrorResponse {
     private String message;
+    private List<ErrorObj> errors;
+
+
+
+    @Data
+    public static class ErrorObj{
+        private String defaultMessage;
+        private String field;
+    }
 }
--- src/main/java/com/payment/service/PaymentService.java
+++ src/main/java/com/payment/service/PaymentService.java
@@ -7,9 +7,12 @@ import com.payment.model.PaymentResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Mono;
+
+import java.util.stream.Collectors;
 
 @Service
 public class PaymentService {
@@ -31,10 +34,25 @@ public class PaymentService {
                         HttpStatusCode::isError,
                         clientResponse -> clientResponse.bodyToMono(ErrorResponse.class)
                                 .flatMap(errorBody -> Mono.error(new ResponseStatusException(
-                                        clientResponse.statusCode(),
-                                        "API error: " + errorBody.getMessage())))
+                                        clientResponse.statusCode(),formatException(errorBody)
+                                        )))
                 )
                 .bodyToMono(PaymentResponse.class)
                 .block();
+    }
+
+    /**
+     * Formats error showing specific field errors
+     * @param response
+     * @return
+     */
+    private static String formatException(ErrorResponse response) {
+        if(CollectionUtils.isEmpty(response.getErrors())){
+            return "API error: " + response.getMessage();
+        }
+        return "API error: " + response.getMessage() + ". Details: " + response.getErrors()
+                .stream()
+                .map(x -> x.getField() + " " + x.getDefaultMessage())
+                .collect(Collectors.joining(","));
     }
 }
